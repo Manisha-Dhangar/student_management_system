9a66901d67a0b12a144bbfe7de885edd4a8c3448
Manisha Dhangar <manishadhangar3237@gmail.com>
2026-06-18 19:19:23 +0530

    add sorting to all list

--- data_store.rb
+++ data_store.rb
@@ -2,5 +2,9 @@ module StudentData
 STUDENTS=[{ id: 1, name: "Manisha", marks: [85, 90, 78, 88, 92, 80], joined_at: "2025-01-15" },
   { id: 2, name: "Ramesh", marks: [45, 55, 60, 50, 58, 62], joined_at: "2025-02-10" },
   { id: 3, name: "Rakesh", marks: [95, 92, 98, 90, 94, 96], joined_at: "2025-03-05" },
-  { id: 4, name: "Aman", marks: [90, 99, 99, 10, 99, 99], joined_at: "2025-01-20" }]
-end
+  { id: 4, name: "Aman",   marks: [90, 99, 99, 10, 99, 99], joined_at: "2025-01-20" },
+  { id: 5,  name: "suman",  marks: [44, 77, 98, 22, 56, 33], joined_at:"2025-04-30"},
+  { id: 6,  name: "shreya", marks: [87, 55, 28, 98, 60, 78], joined_at:"2025-05-24"}
+  ]
+
+  end
--- student_service.rb
+++ student_service.rb
@@ -10,53 +10,56 @@ class StudentService
     def self.search_student
         puts "enter name of student"
         enter_name=gets.chomp
-        if enter_name.match?(/\d/)
+        student_name=enter_name.downcase
+         
+        if student_name.match?(/\d/)
             puts "invalid"
             return
         end
-       
-        if enter_name.empty?
+        if student_name.empty?
             puts "empty string not allowed"
             return
         end
-        student_name=enter_name.downcase
- 
+        check_record=0
         for i in 0..StudentData::STUDENTS.length-1
             count=0
             s_name_from_data=StudentData::STUDENTS[i][:name]
             s_name=s_name_from_data.downcase
-            
-             
             for j in 0..enter_name.length
                 if student_name[j] == s_name[j]
+                    print=true
                     count+=1
+                    check_record+=1
                 end
             end
             
             min_marks=StudentService.find_min?(StudentData::STUDENTS[i][:marks])
             sum_five_marks=StudentService.best_five(StudentData::STUDENTS[i][:marks],min_marks)
-            if count == 0
+            if count == enter_name.length && print
+                puts "Name: #{StudentData::STUDENTS[i][:name]} |   percentage: #{sum_five_marks/5}%  "
+            end 
+        
+            if check_record == 0
                 puts "no record"
-                return
+              return
             end
-
-            if count == enter_name.length
-               
-             puts "Name: #{StudentData::STUDENTS[i][:name]} |   percentage: #{sum_five_marks/5}%  "
-             return
-            end
-           
         end
-    
     end
 
     def self.view_student_tenure
+        student_list=[]
         StudentData::STUDENTS.each do |s|
+            student_hash={}
             current_date=Date.today
             join_date = Date.parse(s[:joined_at])
             days= (current_date - join_date).to_i
-            puts "Name: #{s[:name]}   | Days: #{days}"
+            student_hash[:id]=s[:id]
+            student_hash[:name]=s[:name]
+            student_hash[:days]=days
+            student_list.push(student_hash)
         end
+        sorted_days_list=student_list.sort_by{|l| -l[:days]}
+        puts sorted_days_list
     end
 
     def self.view_grade
@@ -64,16 +67,26 @@ class StudentService
             puts "no record found"
             return
         end
-       
+         student_list=[]
         StudentData::STUDENTS.each do |s|
-         
+           student_hash={}
             min_marks=StudentService.find_min?(s[:marks])
             sum_five_marks=StudentService.best_five(s[:marks],min_marks)
             percentage = sum_five_marks/5
             student_grade = StudentService.find_grade?(percentage)
-
-            puts "Name: #{s[:name]} |  Percentage(%):#{percentage}% |  grade #{student_grade}" 
+            result="pass"
+            if percentage < 33
+               result="fail"
+            end
+            student_hash[:id]=s[:id]
+            student_hash[:name]=s[:name]
+            student_hash[:percentage]=percentage
+            student_hash[:grade]=student_grade
+            student_hash[:result]=result
+            student_list.push(student_hash)
         end
+         student_sorted_grade=student_list.sort_by{|s| -s[:percentage]}
+         puts student_sorted_grade
     end
 
     def self.top_performer
@@ -89,13 +102,15 @@ class StudentService
             sum_five_marks=StudentService.best_five(s[:marks],min_marks)
             students_marks.push(sum_five_marks)
         end
-
+          
         top_mark=StudentService.find_maximum?(students_marks)
+        
         top_student = StudentData::STUDENTS.find do |s|
-            
-          min_marks=StudentService.find_min?(s[:marks])
-             sum_five_marks=StudentService.best_five(s[:marks],min_marks)
+            studennt_hash={}
+            min_marks=StudentService.find_min?(s[:marks])
+            sum_five_marks=StudentService.best_five(s[:marks],min_marks)
             top_mark == sum_five_marks
+
         end
         puts "#{top_student[:name]} marks #{top_mark}"  
     end       
